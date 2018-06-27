5ce46614bf031975c51ea4370f02c240a7eaa9f0
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2018-06-27 22:36:50 +0200

    changed upgrader behaviour

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,7 +2,14 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
+        var upgrade = false;
+        if(creep.carry.energy == creep.carryCapacity){
+            upgrade = true;
+        }else if(creep.carry.energy == 0){
+            upgrade = false;
+        }
+
+	    if(upgrade) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
